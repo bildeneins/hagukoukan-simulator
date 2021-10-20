761211c694fbfb7c21b0f63f1962c92acb19d45f
stonesstones <ku292stones@gmail.com>
2021-10-20 19:00:47 +0900

    シュミレーターのデザインの作成

---
+++ src/components/DrillBtn.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-row v-if="btnNum === 0">
+      <v-col>
+        <v-btn @click="clickStart">
+          {{'開始'}}
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="btnNum === 1">
+      <v-col>
+        <v-btn @click="clickStop">
+          {{'停止'}}
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="btnNum === 2">
+      <v-col>
+        <v-btn>
+          {{'再開'}}
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn>
+          {{'リセット'}}
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "DrillBtn",
+  data(){
+    return {
+      btnNum: 0
+    }
+  },
+  methods: {
+    clickStart(){
+      this.btnNum = 1
+      this.$emit("clickedStartBtn")
+    },
+    clickStop(){
+      this.btnNum = 2
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-tabs>
+      <v-tab
+          @change="onClickedIppanTab"
+      >
+        一般
+      </v-tab>
+      <v-tab
+          @change="onClickedKibanTab"
+      >
+        機番
+      </v-tab>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tab",
+  methods:{
+    onClickedIppanTab(){
+      this.$emit("toIppan")
+    },
+    onClickedKibanTab(){
+      this.$emit("toKiban")
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
